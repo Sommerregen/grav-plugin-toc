9d773c7d180de7632a53adc72478b9c3fc745f3a
-=NeXuS=- <schwartzmann.a@gmail.com>
2021-05-14 14:26:37 +0200

    fix syntax for PHP 8

--- classes/Toc.php
+++ classes/Toc.php
@@ -65,7 +65,7 @@ class Toc
                 }
 
                 // Ignore headings in code, pre or blockquote environments
-                if (!$text || $tag{0} !== 'h') {
+                if (!$text || $tag[0] !== 'h') {
                     continue;
                 }
 
@@ -126,7 +126,7 @@ class Toc
                 $text = trim($match['text']);
 
                 // Don't consider headings in code or pre environments
-                if (($tag{0} !== 'h') || (mb_strlen($text) == 0)) {
+                if (($tag[0] !== 'h') || (mb_strlen($text) == 0)) {
                     // Ignore empty headers, too
                     return $match[0];
                 }
